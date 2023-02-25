c4ab8ae6c5af03a9b6e9f6f8243cb6502def687d
Gregório Neto <igregoriodoneto@gmail.com>
2023-02-25 09:27:21 -0300

    Construção de um CRUD simples com Flask

--- flask-db-docker/app/app.py
+++ flask-db-docker/app/app.py
@@ -21,7 +21,7 @@ def create_table():
   c.close()
 
 @app.route('/')
-def list_task():
+def list():
   c = db()
   cursor = c.cursor()
   cursor.execute('SELECT * FROM tasks;')
@@ -29,5 +29,38 @@ def list_task():
   c.close()
   return render_template('list.html', tasks=tasks)
 
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+  if request.method == 'POST':
+    title = request.form['title']
+    description = request.form['description']
+    c = db()
+    cursor = c.cursor()
+    cursor.execute('INSERT INTO tasks (title, description) values (?, ?)', (title, description))
+    c.commit()
+    c.close()
+    return redirect(url_for('list'))
+  return render_template('create.html')
 
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+  c = db()
+  cursor = c.cursor()
+  cursor.execute('SELECT * FROM tasks WHERE id = ?', (id,))
+  task = cursor.fetchone()
+  if request.method == 'POST':
+    title = request.form['title']
+    description = request.form['description']
+    cursor.execute('UPDATE tasks SET title = ?, description = ? WHERE id = ?', (title, description, id,))
+    c.commit()
+    c.close()
+    return redirect(url_for('list'))
+  c.close()
+  return render_template('edit.html', task=task)
 
+@app.route('/delete/<int:id>')
+def remove(id):
+  c = db()
+  cursor = c.cursor()
+  cursor.execute('DELETE FROM tasks WHERE id = ?', (id))
+  return redirect(url_for(list))
