91653f5af14e7cffa7ac699c4553f4a919868298
Gregório Neto <igregoriodoneto@gmail.com>
2023-03-02 16:12:56 -0300

    Finalizando...

--- cadastro-tarefas/app.py
+++ cadastro-tarefas/app.py
@@ -19,7 +19,7 @@ tarefas = [
 ]
 
 @app.route('/task', methods=['GET', 'POST'])
-def get_tasks():
+def tasks():
   if request.method == 'GET':
     resp = jsonify(tarefas)
     resp.headers.add('Content-Type', 'application/json')
@@ -31,6 +31,25 @@ def get_tasks():
     dados['id'] = posicao
     tarefas.append(dados)
     return jsonify(tarefas[posicao])
+
+@app.route('/task/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def task(id):
+  if request.method == 'GET':
+    try:
+      dados = tarefas[id]
+      return jsonify(dados)
+    except:
+      mensagem = 'Erro na API!'
+      return jsonify({"status": "error", "mesagem": mensagem})
+  elif request.method == 'PUT':
+    dados = json.loads(request.data)
+    task = tarefas[id]
+    print(task)
+    task['status'] = dados['status']
+    return jsonify(task)
+  elif request.method == 'DELETE':
+    tarefas.pop(id)
+    return jsonify({"status": "sucesso", "mensagem": "Tarefa deletada com sucesso!"})
 
 if __name__ == '__main__':
   app.run(debug=True)
