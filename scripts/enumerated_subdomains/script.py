b7c877435718d395642c4c40ca31e8a4432b9c8d
igregorioneto <igregoriodoneto@outlook.com>
2024-02-27 15:23:09 -0300

    fix: refactoring enumerated subdomains

--- scripts/enumerated_subdomains/script.py
+++ scripts/enumerated_subdomains/script.py
@@ -1,26 +1,39 @@
 import sys, dns.resolver
 
-resolver = dns.resolver.Resolver()
+def enum_subdomains(domain, wordlist):
+    resolver = dns.resolver.Resolver()
 
-try:
-    alvo = sys.argv[1]
-    wordlist = sys.argv[2]
-except:
-    print("Usage: python3 script.py dominio wordlist.txt")
-    sys.exit()
-
-try:
-    with open(wordlist, "r") as arq:
-        subdominios = arq.read().splitlines()
-except:
-    print(f"Error ao abrir arquivo {wordlist}")
-    sys.exit()
-
-for subdominio in subdominios:
     try:
-        sub_alvo = "{}.{}".format(subdominio, alvo)
-        resultados = resolver.resolve(sub_alvo, "A")
-        for resultado in resultados:
-            print("{} -> {}".format(sub_alvo,resultado))
-    except:
-        pass
+        with open(wordlist, "r") as arq:
+            subdomains = arq.read().splitlines()
+    except FileNotFoundError:
+        print(f"Error: File '{wordlist}' not found.")
+        return
+    except Exception as e:
+        print(f"Error: {e}")
+        return
+
+    for subdomain in subdomains:
+        try:
+            target = f"{subdomain}.{domain}"
+            results = resolver.resolve(target, "A")
+            for result in results:
+                print(f"{target} -> {result}")
+        except dns.resolver.NoAnswer:
+            print(f"No DNS records found for '{target}'")
+        except dns.resolver.NXDOMAIN:
+            print(f"DNS domain '{target}' does not exist")
+        except dns.resolver.NoNameservers:
+            print(f"No nameservers found")
+        except Exception as e:
+            print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python3 script.py <domain> <wordlist>")
+        sys.exit()
+
+    domain = sys.argv[1]
+    wordlist = sys.argv[2]
+    enum_subdomains(domain, wordlist)
