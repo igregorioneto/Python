765356e36870f97cadf5fd04c3abc56e935689ca
igregorioneto <igregoriodoneto@outlook.com>
2024-03-03 09:58:09 -0300

    feat: grafos deteccao

---
+++ language/dsa/fundamentals/grafos/deteccao.py
@@ -0,0 +1,61 @@
+'''
+Sistema de detecção de intrusões para encontrar o menor caminho
+mais curto entre um computador comprometido e um computador
+crítico na rede.
+'''
+
+import heapq
+
+class Grafo:
+    def __init__(self):
+        self.vertices = {}
+
+    def adicionar_vertices(self, vertice):
+        if vertice not in self.vertices:
+            self.vertices[vertice] = {}
+
+    def adicionar_aresta(self, origem, destino, peso):
+        if origem in self.vertices and destino in self.vertices:
+            self.vertices[origem][destino] = peso
+
+    def dijkstra(self, origem, destino):
+        distancia = {v: float('inf') for v in self.vertices}
+        distancia[origem] = 0
+        fila = [(0, origem)]
+
+        while fila:
+            # Remove e retorna o vértice com a menor distância da fila de propriedade
+            dist_v, v = heapq.heappop(fila)
+            if v == destino:
+                return distancia[v]
+            if dist_v > distancia[v]:
+                continue
+            for u, peso in self.vertices[v].items():
+                nova_dist = dist_v + peso
+                if nova_dist < distancia[u]:
+                    distancia[u] = nova_dist
+                    heapq.heappush(fila, (nova_dist, u))
+        
+        return float('inf')
+
+# Exemple of use    
+if __name__ == "__main__":
+    grafo = Grafo()
+    grafo.adicionar_vertices('A')
+    grafo.adicionar_vertices('B')
+    grafo.adicionar_vertices('C')
+    grafo.adicionar_vertices('D')
+    grafo.adicionar_vertices('E')
+    grafo.adicionar_vertices('F')
+
+    grafo.adicionar_aresta('A', 'B', 1)
+    grafo.adicionar_aresta('A', 'C', 2)
+    grafo.adicionar_aresta('B', 'D', 3)
+    grafo.adicionar_aresta('C', 'E', 4)
+    grafo.adicionar_aresta('D', 'F', 5)
+    grafo.adicionar_aresta('E', 'F', 6)
+
+    origem = 'A'
+    destino = 'F'
+    menor_caminho = grafo.dijkstra(origem, destino)
+    print(f"O menor caminho entre {origem} e {destino} é de {menor_caminho}")
