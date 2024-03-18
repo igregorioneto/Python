a09547654e543cc56ce750f8fdf1e2e966f520c0
igregorioneto <igregoriodoneto@outlook.com>
2024-03-18 14:27:31 -0300

    feat: insert delete getrandom

--- language/dsa/exercises/insert_delete_getrandom_o1.py
+++ language/dsa/exercises/insert_delete_getrandom_o1.py
@@ -1,6 +1,6 @@
 import random
 
-class RandomizedSet:
+class RandomizedSet2:
 
     def __init__(self):
         self.set = []
@@ -24,6 +24,41 @@ class RandomizedSet:
 
     def getRandom(self) -> int:
         return random.choice(self.set)
+    
+class RandomizedSet:
+
+    def __init__(self):
+        self.set = set()
+        self.val_to_index = {}
+        self.val_to_list = []
+        
+
+    def insert(self, val: int) -> bool:
+        if val in self.set:
+            return False
+        
+        self.set.add(val)
+        self.val_to_list.append(val)
+        self.val_to_index[val] = len(self.val_to_list) - 1
+        return True
+        
+
+    def remove(self, val: int) -> bool:
+        if val not in self.set:
+            return False
+        
+        idx = self.val_to_index[val]
+        last_val = self.val_to_list[-1]
+        self.val_to_list[idx] = last_val
+        self.val_to_index[last_val] = idx
+        self.val_to_list.pop()
+        del self.val_to_index[val]
+        self.set.remove(val)
+        return True
+        
+
+    def getRandom(self) -> int:
+        return random.choice(self.val_to_list)
     
 if __name__ == "__main__":
     solution = RandomizedSet()
