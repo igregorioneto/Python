e328dfba127feac0f7aa9eca75a4e4f4ad30e961
igregorioneto <igregoriodoneto@outlook.com>
2024-03-20 09:31:06 -0300

    feat: handle input

--- pygame/snake/game.py
+++ pygame/snake/game.py
@@ -10,15 +10,16 @@ SNAKE_COLOR = (0,255,0)
 APPLE_COLOR = (255,0,0)
 SNAKE_WIDTH = 20
 APPLE_WIDTH = 20
-SNAKE_SPEED = 10
+SNAKE_SPEED = 5
 
-
+# Initialize Game
 def initialize_game():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("SNAKE")
     return screen
 
+# Draw elements
 def draw_elements(screen,snake, apple):
     screen.fill(BACKGROUND)
     pygame.draw.rect(screen, SNAKE_COLOR, snake)
@@ -26,9 +27,22 @@ def draw_elements(screen,snake, apple):
 
     pygame.display.flip()
 
+# Random int
 def random_value(initial, value):
     return random.randint(initial,value)
 
+# Moviment Snake
+def handle_input(snake):
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        snake.y -= SNAKE_SPEED
+    if keys[pygame.K_DOWN]:
+        snake.y += SNAKE_SPEED
+    if keys[pygame.K_LEFT]:
+        snake.x -= SNAKE_SPEED
+    if keys[pygame.K_RIGHT]:
+        snake.x += SNAKE_SPEED
+    
 
 def main():
     screen = initialize_game()
@@ -47,6 +61,10 @@ def main():
                 sys.exit()
 
         draw_elements(screen, snake, apple)
+
+        handle_input(snake)
+
+        clock.tick(60)
 
 
 if __name__ == "__main__":
