07e9436b6f28399dfc14a97a84821a9b1a3b1951
igregorioneto <igregoriodoneto@outlook.com>
2024-03-20 09:53:33 -0300

    feat: updating snake, colision

--- pygame/snake/game.py
+++ pygame/snake/game.py
@@ -15,9 +15,11 @@ SNAKE_SPEED = 5
 # Initialize Game
 def initialize_game():
     pygame.init()
+    pygame.mixer.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("SNAKE")
-    return screen
+    coin = pygame.mixer.Sound('assets/smw_coin.wav')
+    return screen, coin
 
 # Draw elements
 def draw_elements(screen,snake, apple):
@@ -32,6 +34,7 @@ def random_value(initial, value):
     return random.randint(initial,value)
 
 # Moviment Snake
+# Moviment like the original game
 def handle_input(snake):
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP] and not keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
@@ -44,10 +47,29 @@ def handle_input(snake):
         snake.x += SNAKE_SPEED
     
 
+def colision(snake, apple, sound, points):
+    if snake.colliderect(apple):
+        sound.play()
+        points += 1
+        apple.x = random_value(0, SCREEN_WIDTH - APPLE_WIDTH)
+        apple.y = random_value(0, SCREEN_HEIGHT - APPLE_WIDTH)
+
+def updating_snake(snake):
+    if snake.x < 0:
+        snake.x = SCREEN_WIDTH - SNAKE_WIDTH
+    if snake.x > SCREEN_WIDTH:
+        snake.x = 0
+    if snake.y < 0:
+        snake.y = SCREEN_HEIGHT - SNAKE_WIDTH
+    if snake.y > SCREEN_HEIGHT:
+        snake.y = 0
+
 def main():
-    screen = initialize_game()
+    screen, coin = initialize_game()
     clock = pygame.time.Clock()
     font = pygame.font.Font(None, FONT_SIZE)
+
+    points = 0
 
     # Snake
     snake = pygame.Rect(50, SCREEN_HEIGHT // 2 - SNAKE_WIDTH // 2, SNAKE_WIDTH, SNAKE_WIDTH)
@@ -63,6 +85,10 @@ def main():
         draw_elements(screen, snake, apple)
 
         handle_input(snake)
+
+        colision(snake, apple, coin, points)
+
+        updating_snake(snake)
 
         clock.tick(60)
 
