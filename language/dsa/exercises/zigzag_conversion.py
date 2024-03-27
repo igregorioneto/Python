37d57e7469f61c93a36bf459049cdb6dd49a41a0
igregorioneto <igregoriodoneto@outlook.com>
2024-03-27 09:12:02 -0300

    feat: zigzag conversion

---
+++ language/dsa/exercises/zigzag_conversion.py
@@ -0,0 +1,18 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows > len(s):
+            return s
+        result = [''] * numRows
+        index, step = 0, 1
+        for char in s:
+            result[index] += char
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+            index += step
+        return ''.join(result)
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.convert(s = "PAYPALISHIRING", numRows = 3))
