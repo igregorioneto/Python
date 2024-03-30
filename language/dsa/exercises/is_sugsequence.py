82826d785a838b89fc51a531fb9b0085724f875f
igregorioneto <igregoriodoneto@outlook.com>
2024-03-30 10:14:27 -0300

    feat: is subsequence 2

--- language/dsa/exercises/is_sugsequence.py
+++ language/dsa/exercises/is_sugsequence.py
@@ -1,10 +1,17 @@
 class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
+    def isSubsequence2(self, s: str, t: str) -> bool:
         i, j = 0, 0
         while i < len(s) and j < len(t):
             if s[i] == t[j]:
                 i += 1
             j += 1
+        return i == len(s)
+
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = 0
+        for char in t:
+            if i < len(s) and char == s[i]:
+                i += 1
         return i == len(s)
     
 if __name__ == "__main__":
