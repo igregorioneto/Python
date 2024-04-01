fa7f08f14a7dfb57c3ce2fe7e9213f8ba3546298
igregorioneto <igregoriodoneto@outlook.com>
2024-04-01 16:12:53 -0300

    feat: Bee object and Text

--- pygame/bee_honey/obj.py
+++ pygame/bee_honey/obj.py
@@ -10,5 +10,59 @@ class Obj:
         self.sprite.rect[0] = x
         self.sprite.rect[1] = y
 
+        self.frame = 1
+        self.tick = 0
+
     def drawing(self, window):
         self.group.draw(window)
+
+    def anim(self, image, tick, frames):
+        self.tick += 1
+
+        if self.tick == tick:
+            self.tick = 0
+            self.frame += 1
+
+        if self.frame == frames:
+            self.frame = 1
+
+        self.sprite.image = pygame.image.load(f"assets/{image}{self.frame}.png")
+
+
+class Bee(Obj):
+    def __init__(self, image, x, y):
+        super.__init__(self,image, x, y)
+
+        pygame.mixer.init()
+        self.sound_pts = pygame.mixer.Sound("assets/sounds/score.ogg")
+        self.sound_block = pygame.mixer.Sound("assets/sounds/bateu.ogg")
+
+        self.life = 3
+        self.pts = 0
+
+    def move_bee(self, event):
+        if event.type == pygame.MOUSEMOTION:
+            self.sprite.rect[0] = pygame.mouse.get_pos()[0] - 35
+            self.sprite.rect[1] = pygame.mouse.get_pos()[1] - 30
+
+    def colision(self, group, name):
+        name = name
+        colison = pygame.sprite.spritecollide(self.sprite, group, True)
+
+        if name == "Flower" and colison:
+            self.pts += 1
+            self.sound_pts.play()
+        elif name == "Spider" and colison:
+            self.life -= 1
+            self.sound_block.play()
+
+class Text:
+    def __init__(self, size, text):
+        self.font = pygame.font.SysFont("Arial bold", size)
+        self.render = self.font.render(text, False, (255,255,255))
+
+    def draw(self, window, x, y):
+        window.blit(self.render, (x, y))
+
+    def update_text(self, update):
+        self.render = self.font.render(update, False, (255,255,255))
