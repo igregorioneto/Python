49b82b5ffe7c2d380ca1ce89a43b7256ad30423c
igregorioneto <igregoriodoneto@outlook.com>
2024-04-08 15:59:46 -0300

    feat: Surfaces

--- pygame/asteroid_shooter/main.py
+++ pygame/asteroid_shooter/main.py
@@ -6,10 +6,21 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 640
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Asteroid Shooter")
 
+# creating a surface
+test_surf = pygame.Surface((400,100))
+
 while True:
-    for event in pygame.event.get():
+    # 1. Inputs
+    for event in pygame.event.get():        
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    # 2. updates
+    display_surface.fill((15, 140, 122))
+    test_surf.fill((186, 120, 39))
+
+    display_surface.blit(test_surf, (0,0))
+
+    # 3. show the frame
     pygame.display.update()
