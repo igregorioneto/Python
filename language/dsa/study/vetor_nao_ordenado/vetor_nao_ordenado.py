dc42f43b3239b7ad55489272956fac61e3d270c2
igregorioneto <igregoriodoneto@outlook.com>
2024-04-08 14:39:59 -0300

    feat: criando o redimensionar vetor no velor não ordenado

--- language/dsa/study/vetor_nao_ordenado/vetor_nao_ordenado.py
+++ language/dsa/study/vetor_nao_ordenado/vetor_nao_ordenado.py
@@ -14,9 +14,10 @@ class VetorNaoOrdenado:
             for i in range(self.ultima_posicao + 1):
                 print(i, ' - ', self.valores[i])
 
-    def inserir(self, valor):
+    def inserir(self, valor):  
+        self.redimensionar()             
         if self.ultima_posicao == self.capacidade - 1:
-            print('Capacidade máxima atingida')
+            print('Capacidade máxima atingida')            
         else:
             self.ultima_posicao += 1
             self.valores[self.ultima_posicao] = valor
@@ -37,29 +38,58 @@ class VetorNaoOrdenado:
             
             self.ultima_posicao -= 1
 
+    def redimensionar(self):
+        if self.ultima_posicao == self.capacidade - 1:
+           nova_capacidade = self.capacidade * 2
+           novo_vetor = np.empty(nova_capacidade, dtype=int)
+
+           for i in range(self.capacidade):
+               novo_vetor[i] = self.valores[i]
+
+           self.valores = novo_vetor
+           self.capacidade = nova_capacidade
 
 
-
-vetor = VetorNaoOrdenado(10)
+vetor = VetorNaoOrdenado(5)
 vetor.imprime()
-
+print('-----')
 vetor.inserir(1)
 vetor.inserir(2)
 vetor.inserir(3)
 vetor.inserir(4)
 vetor.inserir(5)
+vetor.inserir(5)
 vetor.imprime()
+print('-----')
 
 print(vetor.pesquisar(3))
+print('-----')
 print(vetor.pesquisar(5))
+print('-----')
 
 vetor.excluir(3)
 vetor.imprime()
+print('-----')
 
 vetor.excluir(5)
 vetor.imprime()
+print('-----')
 
 vetor.excluir(1)
+vetor.imprime()
+print('-----')
+vetor.inserir(1)
+vetor.inserir(2)
+vetor.inserir(3)
+vetor.inserir(4)
+vetor.inserir(5)
+vetor.inserir(5)
+vetor.inserir(1)
+vetor.inserir(2)
+vetor.inserir(3)
+vetor.inserir(4)
+vetor.inserir(5)
+vetor.inserir(5)
 vetor.imprime()
 
 
