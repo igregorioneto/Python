07ff3eb00f7ef57ce58d556dd60ea3bdc948951c
igregorioneto <igregoriodoneto@outlook.com>
2024-04-08 20:17:46 -0300

    feat: moviment

--- pygame/asteroid_shooter/code/main.py
+++ pygame/asteroid_shooter/code/main.py
@@ -6,6 +6,8 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 640
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Asteroid Shooter")
 
+clock = pygame.time.Clock()
+
 # creating a surface
 # test_surf = pygame.Surface((400,100))
 ship_surf = pygame.image.load('graphics/ship.png').convert_alpha()
@@ -15,6 +17,8 @@ bg_surf = pygame.image.load('graphics/background.png').convert_alpha()
 font = pygame.font.Font('graphics/subatomic.ttf', 50)
 font_surf = font.render('Space', True, (255, 255, 255))
 
+ship_y_pos = 500 
+
 while True:
     # 1. Inputs
     for event in pygame.event.get():        
@@ -22,12 +26,15 @@ while True:
             pygame.quit()
             sys.exit()
 
+    clock.tick(120)
+
     # 2. updates
     display_surface.fill((0, 0, 0))
     # test_surf.fill((186, 120, 39))
     display_surface.blit(bg_surf, (0,0))
-    display_surface.blit(ship_surf, (300,500))
-    display_surface.blit(font_surf, (WINDOW_WIDTH // 2 - font_surf.get_width() // 2, 500))
+    ship_y_pos -= 4
+    display_surface.blit(ship_surf, (300,ship_y_pos))
+    display_surface.blit(font_surf, (WINDOW_WIDTH // 2 - font_surf.get_width() // 2, 200))
 
     # display_surface.blit(test_surf, dest=(WINDOW_WIDTH - test_surf.get_width(),0))
 
